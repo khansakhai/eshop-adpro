4a79b45bf14020ffadd49d45b5756779acf371e0
khansakhai <kak.khansa@gmail.com>
2025-02-12 00:38:27 +0700

    feat: add edit functionality

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -6,17 +6,38 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
     public Product create(Product product) {
+        product.setProductId(UUID.randomUUID().toString());
         productData.add(product);
         return product;
     }
 
     public Iterator<Product> findAll() {
         return productData.iterator();
+    }
+
+    public Product findById(String id) {
+        return productData.stream()
+                .filter(product -> product.getProductId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public Product edit(Product editedProduct) {
+        if (editedProduct.getProductId() == null) return null;
+
+        for (int i = 0; i < productData.size(); i++) {
+            if (productData.get(i).getProductId().equals(editedProduct.getProductId())) {
+                productData.set(i, editedProduct);
+                return editedProduct;
+            }
+        }
+        return null;
     }
 }
