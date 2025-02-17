02470652f90697b829c5d51aef2cec2049a1ed49
khansakhai <kak.khansa@gmail.com>
2025-02-18 00:02:03 +0700

    fix: change EshopApplication modifier to protected

--- src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EshopApplication {
 
-    private EshopApplication() {
+    protected EshopApplication() {
 
     }
 
