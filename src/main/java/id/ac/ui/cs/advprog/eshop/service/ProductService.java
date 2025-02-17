b29d61e5013fc552a72c824bcc92ec0b3416ad8c
khansakhai <kak.khansa@gmail.com>
2025-02-17 23:55:35 +0700

    fix pmd: remove unnecessary modifier in ProductService

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -4,9 +4,9 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 
 public interface ProductService {
-    public Product create(Product product);
-    public List<Product> findAll();
-    public Product findById(String id);
-    public Product edit(Product product);
-    public boolean delete(String id);
+    Product create(Product product);
+    List<Product> findAll();
+    Product findById(String id);
+    Product edit(Product product);
+    boolean delete(String id);
 }
