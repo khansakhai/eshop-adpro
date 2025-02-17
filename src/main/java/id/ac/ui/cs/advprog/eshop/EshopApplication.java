f2003202d2ec6cd26ab20ebadb3dc7c6863ee428
khansakhai <kak.khansa@gmail.com>
2025-02-17 23:37:41 +0700

    fix pmd: add private constructor to EshopApplication

--- src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EshopApplication {
 
+    private EshopApplication() {
+
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(EshopApplication.class, args);
     }
