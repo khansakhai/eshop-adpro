d6095b5c2a79195319f22947eec077023bac8ba7
khansakhai <kak.khansa@gmail.com>
2025-02-18 00:10:18 +0700

    fix: revert EshopApplication to initial state

--- src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
@@ -6,10 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EshopApplication {
 
-    protected EshopApplication() {
-
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(EshopApplication.class, args);
     }
