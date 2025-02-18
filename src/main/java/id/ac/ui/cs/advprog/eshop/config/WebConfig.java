13d6655b12e5edf9fcc417326ba7e7a74d37f7a1
khansakhai <kak.khansa@gmail.com>
2025-02-18 14:22:39 +0700

    feat: add config

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/config/WebConfig.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.eshop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/product/list");
+    }
+}
